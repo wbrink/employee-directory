fee93bd4d8bd74f6c7522c3640a49d624e9e44dd
wbrink <will.brink55@gmail.com>
2020-06-09 12:06:10 -0700

    added styling for search bar

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,12 +1,16 @@
 import React, {useContext} from "react";
 import SearchContext from "../../SearchContext";
+import "./style.css";
 
 export default function Search(props) {
 
   const {search, setSearch} = useContext(SearchContext);
 
   return (
-    <h3>Search</h3>
+    <div className="form">
+      <label htmlFor="search">Search Here</label>
+      <input type="text" id="search"/>
+    </div>
+    
   )
 }
-
